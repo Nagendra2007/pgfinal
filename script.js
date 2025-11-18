466a8dc9c93f3f7df61f01db3d57f1623a9b73f7
Nagendra2007 <nagendraambati859@gmail.com>
2025-11-18 11:13:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let cross = document.querySelector('#cross');
 let sidemenu = document.querySelector('.sidemenu');
+let overlay = document.querySelector('.overlay');
+let ham = document.querySelector("#hamburger");
+ham.addEventListener('click',()=>{
+        sidemenu.classList.add("sideanim");
+        overlay.style.display="block"
+})
  cross.addEventListener('click',()=>{
-       sidemenu.classList.add("sideanim");
+       overlay.style.display="none";
  })
