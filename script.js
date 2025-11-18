93bd415926243061b50b6eeff6c1e14d876a0db3
Nagendra2007 <nagendraambati859@gmail.com>
2025-11-18 11:02:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let cross = document.querySelector('#cross');
+let sidemenu = document.querySelector('.sidemenu');
+ cross.addEventListener('click',()=>{
+       sidemenu.classList.add("sideanim");
+ })
